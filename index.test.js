41633acec5bb92dd8349e892daded7cb2e74438a
Andrei Calazans <andreixoc@hotmail.com>
2021-03-09 14:23:19 -0300

    Tests: Object is a function

--- index.test.js
+++ index.test.js
@@ -2,4 +2,8 @@ describe("G2i Tests", () => {
   test("it can pass", () => {
     expect(true).toBe(true);
   });
+
+  test("Object is a function", () => {
+    expect(typeof Object).toBe("function");
+  });
 });
