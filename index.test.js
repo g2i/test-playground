6e06da5dcb5b4d9d680ce51afbcf836f3599aeba
Dmitry Selikhov <selikhov.dmitrey@gmail.com>
2021-07-01 17:00:35 -0400

    dmitry-selikhov: Add unit test

--- index.test.js
+++ index.test.js
@@ -14,4 +14,8 @@ describe("G2i Tests", () => {
   test("My Name is John", () => {
     expect("John").toBe("John");
   });
+
+  test("My Name is Dmitry", () => {
+    expect("Dmitry").toBe("Dmitry");
+  });
 });
